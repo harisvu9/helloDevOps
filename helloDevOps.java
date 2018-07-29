1b92fe28c715120c27d92ea5dd7aa6345fa8e563
Hari Babu Boppudi <hbopp93@students.campbellsville.edu>
2018-07-29 13:56:10 -0700

    Hello DevOps

---
+++ helloDevOps.java
@@ -0,0 +1,8 @@
+public class helloDevOps {
+
+    public static void main(String[] args) {
+        // Prints "Hello, DevOps" to the terminal window.
+        System.out.println("Hello, DevOps");
+    }
+
+}
